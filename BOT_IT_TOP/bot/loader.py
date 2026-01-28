29c0926c23b3934b4770c0c100268bc5755ed2d7
Farkyaa <139054701+Farkyaa@users.noreply.github.com>
2026-01-28 08:18:35 +0300

    Update loader.py

--- BOT_IT_TOP/bot/loader.py
+++ BOT_IT_TOP/bot/loader.py
@@ -1,6 +1,17 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
+from config import BOT_TOKEN
+from handlers.start import router as start_router
+from handlers.reports import router as reports_router
+
+from aiogram.client.default import DefaultBotProperties
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML")
+)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+dp.include_router(start_router)
+dp.include_router(reports_router)
